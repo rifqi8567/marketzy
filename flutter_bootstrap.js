04a21e0f47576defc5d11267eda4a9886be00245
rifqi856 <rifqithufail1208@gmail.com>
2025-05-09 20:53:49 +0700

    deploy v13

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2555660515"
+    serviceWorkerVersion: "2425029985"
   }
 });
